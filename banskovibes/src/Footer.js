00fce4605c904f1ed55df811838ff6bf461a2e7a
olilchev <o.lilchev@gmail.com>
2023-03-31 18:57:50 +0300

    Create Footer.js
    Created Footer componen

---
+++ banskovibes/src/Footer.js
@@ -0,0 +1,27 @@
+import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
+export const Footer = () => {
+    return (
+      <div className="footer-container">
+        <footer className="bg-dark-green text-white position-fixed" style={{ bottom: 0, left: 0, right: 0, width: "100%" }}>
+          <div className="container">
+            <div className="row mt-auto">
+              <div className="col-lg-12 text-center">
+                <a href=""><FontAwesomeIcon icon={faFacebook} className="footer-icon" color="white" size="2x" /></a>
+                <a href=""><FontAwesomeIcon icon={faWhatsapp} className="footer-icon" color="white" size="2x" /></a>
+                <a href=""><FontAwesomeIcon icon={faInstagram} className="footer-icon" color="white" size="2x" /></a>
+              </div>
+              <div className="col-lg-12 text-center mt-3">
+                <p>
+                  © {new Date().getFullYear()} Bansko Vibes Apartments. All rights reserved.
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    );
+  };
