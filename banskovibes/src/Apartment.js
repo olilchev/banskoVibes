82b86fb593a93ab1798a217c37f838034ce3e032
olilchev <o.lilchev@gmail.com>
2023-04-02 03:24:36 +0300

    Update Apartment.js

--- banskovibes/src/Apartment.js
+++ banskovibes/src/Apartment.js
@@ -1,6 +1,6 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faKitchenSet, faBath, faUtensils, faDoorOpen, faShower, faPerson,faChild } from '@fortawesome/free-solid-svg-icons'
+import { faKitchenSet, faBath, faUtensils, faDoorOpen, faShower, faPerson,faChild, faCheck } from '@fortawesome/free-solid-svg-icons'
 
 import {
 
@@ -26,16 +26,17 @@ export const Apartment = () => {
 
     return (
 <>
-<div style={{ display: 'flex', flexWrap: 'wrap' }}>
+{/* <div style={{ display: 'flex', flexWrap: 'wrap' }}> */}
 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-<div>
+<div style={{ display: 'flex' }}>
 <Card id = "flora"
   style={{
     display: 'block',
     width: '18rem',
     padding: 30,
     marginTop: "30px",
-    border: 'none' 
+    border: 'none' ,
+    marginLeft: "30px"
     // "30px 60px 30px 45px"
   }}
     
@@ -80,8 +81,6 @@ export const Apartment = () => {
     
   </CardBody>
 </Card>
-</div>
-<div>
 <Card id="vesta"
   style={{
     display: 'block',
@@ -136,7 +135,54 @@ Comfort, mountain view and great location
     
   </CardBody>
 </Card>
+<Card id="info"
+  style={{
+    display: 'block',
+    width: '31rem',
+    padding: 30,
+    marginTop: "40px",
+    border: 'none',
+    marginLeft: "1px" 
+    
+    // "30px 60px 30px 45px"
+  }}
+    
+ 
+>
+  <CardBody>
+
+    
+    <CardSubtitle
+  
+  style={{ color: "#193e3c", display: "flex", justifyContent: "center", fontSize: "20px", fontWeight: "bold" }}
+>
+
+Fully equipped kitchen <br />
+          Proffessional cleaning and desinfection<br />
+          Air conditioning and heating <br />
+          High-speed Wi-Fi <br />
+          
+          Queen size bed <br />
+          Cleaning on request<br />
+          Laundry inside<br />
+          Free parking <br />
+         
+          Fresh towels and linen<br />
+          Transfers<br />
+          Good option for digital nomads!<br />
+
+</CardSubtitle>
+  </CardBody>
+ 
+  <CardBody>
+
+    
+  </CardBody>
+</Card>
+
 </div>
+<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+<h2>Profesionally cleaning and desinfection</h2>
 </div>
 </div>
 </>
